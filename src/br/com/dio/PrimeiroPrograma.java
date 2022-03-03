7a1b6fa89d032e0a983196140d2508b87de03545
Allcidez <allcidez@yahoo.com.br>
2022-03-03 16:11:48 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+/*        int a = 3;
+        int b = 6;
+        System.out.println("Olá Mundo java!!!"+(a+b));*/
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+
+    }
+
+}
